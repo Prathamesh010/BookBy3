bb49cf19e59d02bfca592f68cc041cb748001e70
Prathamesh010 <prathameshgawas87@gmail.com>
2022-11-23 21:49:46 +0530

    Feat(Client): Add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,31 @@
 <template>
   <div id="app">
-    <div v-if="$store.state.loading" class="loader">
-      <div class="loader__inner"></div>
-    </div>
-    <h1>{{ $store.state.auth.user.username }}</h1>
-    <p v-if="$store.state.error">{{ $store.state.error }}</p>
-    <Login />
-    <!-- <Register /> -->
+    <Navbar />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Login from './components/Login.vue';
-
+import Navbar from './components/Navbar.vue';
 export default {
   name: 'App',
+
   components: {
-    Login,
+    Navbar,
   },
+
+  data: () => ({}),
 };
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.loader {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(255, 255, 255, 0.8);
-  z-index: 9999;
-}
-
-.loader__inner {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 50px;
-  height: 50px;
-  margin-top: -25px;
-  margin-left: -25px;
-  border: 3px solid #ccc;
-  border-top-color: #3498db;
-  border-radius: 50%;
-  animation: spin 1s linear infinite;
-}
-
-@keyframes spin {
-  to {
-    -webkit-transform: rotate(360deg);
-  }
+body {
+  font-family: 'Poppins', sans-serif;
+  background-color: #131418;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="navbar">
+    <div class="navbar__content">
+      <div class="navbar__logo">
+        <img src="../assets/logo.svg" alt="logo" />
+      </div>
+      <div class="navbar__links">
+        <a href="#">Home</a>
+        <a href="#">BOOK/3</a>
+        <a href="#">Resources</a>
+        <a href="#">Syllabus</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+    <div class="navbar__buttons">
+      <button class="navbar__buttons--login">Login</button>
+      <button class="navbar__buttons--signup">Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+  background-color: #131418;
+  height: 5rem;
+}
+
+.navbar__content {
+  display: flex;
+  align-items: center;
+}
+
+.navbar__logo {
+  display: flex;
+  align-items: center;
+}
+
+.navbar__logo img {
+  width: 4rem;
+}
+
+.navbar__links {
+  display: flex;
+  align-items: center;
+  margin-left: 2rem;
+}
+
+.navbar__links a {
+  margin: 0 1.5rem;
+  color: #fff;
+  text-decoration: none;
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+
+.navbar__links a:hover {
+  color: #f50057;
+}
+
+.navbar__links a.active {
+  color: #f50057;
+}
+
+.navbar__links a.active:hover {
+  color: #fff;
+}
+
+.navbar__buttons {
+  display: flex;
+  align-items: center;
+}
+
+.navbar__buttons--login {
+  margin-right: 1rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 0.2rem;
+  background-color: #f50057;
+  color: #fff;
+  font-size: 1.2rem;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+.navbar__buttons--signup {
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 0.2rem;
+  background-color: #fff;
+  color: #131418;
+  font-size: 1.2rem;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+.navbar__buttons--signup:hover {
+  background-color: #f50057;
+  color: #fff;
+}
+
+@media screen and (max-width: 768px) {
+  .navbar {
+    padding: 0 1rem;
+  }
+
+  .navbar__links {
+    display: none;
+  }
+}
+</style>
