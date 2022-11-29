88e41e344a5465f69f570a0b231e8245a4ed833f
Prathamesh010 <prathameshgawas87@gmail.com>
2022-11-29 09:05:55 +0530

    Fix(Backend): Add user field in syllabus schema

--- backend/models/syllabus.js
+++ backend/models/syllabus.js
@@ -18,6 +18,10 @@ const syllabusSchema = new mongoose.Schema({
     file: {
         type: String,
         required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
